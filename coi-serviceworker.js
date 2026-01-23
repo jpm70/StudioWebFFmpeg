46f6387272449b8d7842d91502de2c6e6045450b
José Pérez <jpm70a@gmail.com>
2026-01-23 17:10:15 +0100

    Update coi-serviceworker.js

--- coi-serviceworker.js
+++ coi-serviceworker.js
@@ -1,39 +1,24 @@
-/*! coi-serviceworker v0.1.7 - MIT License - https://github.com/gzuidhof/coi-serviceworker */
 if (typeof window === "undefined") {
     self.addEventListener("install", () => self.skipWaiting());
     self.addEventListener("activate", (event) => event.waitUntil(self.clients.claim()));
-
     self.addEventListener("fetch", (event) => {
-        if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") {
-            return;
-        }
-
+        if (event.request.cache === "only-if-cached" && event.request.mode !== "same-origin") return;
         event.respondWith(
             fetch(event.request).then((response) => {
                 if (response.status === 0) return response;
-
                 const newHeaders = new Headers(response.headers);
                 newHeaders.set("Cross-Origin-Embedder-Policy", "require-corp");
                 newHeaders.set("Cross-Origin-Opener-Policy", "same-origin");
-
-                return new Response(response.body, {
-                    status: response.status,
-                    statusText: response.statusText,
-                    headers: newHeaders,
-                });
+                return new Response(response.body, { status: response.status, statusText: response.statusText, headers: newHeaders });
             })
         );
     });
 } else {
     const script = document.currentScript;
     if (window.crossOriginIsolated === false) {
-        navigator.serviceWorker.register(script.src).then((registration) => {
-            registration.addEventListener("updatefound", () => {
-                location.reload();
-            });
-            if (registration.active) {
-                location.reload();
-            }
+        navigator.serviceWorker.register(script.src).then((reg) => {
+            reg.addEventListener("updatefound", () => location.reload());
+            if (reg.active) location.reload();
         });
     }
 }
